2643de5b64fa4f1715dfd713d8d7c7b6bf291ce4
ikuma <97657938+ikkundayo@users.noreply.github.com>
2022-05-23 22:20:26 +0900

    save

---
+++ first_project/first_project/basic/base2.py
@@ -0,0 +1,10 @@
+animal = 'dog'
+動物 = 'cat'
+print(animal)
+
+age = input('how old are you :')
+
+if int(age) < 20:
+  print('未成年')
+else:
+  print('成人')
---
+++ first_project/first_project/basic/basic.py
@@ -0,0 +1,9 @@
+print('Hello')
+
+a = b = 'happy'
+print(b)
+
+old = input('how old are you :')
+print('you are age is {}' .format(old))
+
+print(a.b)
